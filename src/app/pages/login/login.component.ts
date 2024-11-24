6ccd7c77400b69d167095815856098d9876683ff
imarthin <admin@imarthin.com>
2024-11-24 01:59:02 -0500

    actualizado 24_11_24

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,13 +13,15 @@ export class LoginComponent {
 
   switchToRegister(event: Event): void {
     event.preventDefault();
+    console.log('Cambiando a formulario de registro');
     this.isLogin = false; // Cambiar al formulario de registro
 
-}
+  }
 
-switchToLogin(event: Event): void {
+  switchToLogin(event: Event): void {
   event.preventDefault();
+  console.log('Cambiando a formulario de inicio de sesión');
   this.isLogin = true; // Cambiar al formulario de inicio de sesión
-}
+  }
 }
 
