d21870c0d7480ca45a2d28b62a622bd20a4fed4d
Cdantepedroza <christiandantepi@gmail.com>
2024-11-25 02:28:52 -0500

    flujo inicio/busqueda/reserva

--- src/app/pages/reserva/reserva.component.ts
+++ src/app/pages/reserva/reserva.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-reserva',
   standalone: true,
@@ -8,29 +9,33 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './reserva.component.html',
   styleUrl: './reserva.component.css'
 })
-export class ReservaComponent {
+export class ReservaComponent implements OnInit {
+
   showConfirmation: boolean = false;
-
-
+  
   confirmReservation(): void {
     this.showConfirmation = true; 
   }
 
   nombre: string = '';
   direccion: string = '';
-  descripcion: string = '';
   precio: number = 0;
+  selectedDate: string = '';
+  selectedTime: string = '';
+  
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+
+  ) {}
 
   ngOnInit(): void {
-
+    // Obtener datos de la ruta (queryParams) si es necesario
     this.route.queryParams.subscribe(params => {
-      this.nombre = params['nombre'] || '';
+      this.nombre = params['nombre'] || '';  // Asegúrate de que el parámetro esté presente
       this.direccion = params['direccion'] || '';
-      this.descripcion = params['descripcion'] || '';
-      this.precio = params['precio'] || 0;
+      this.precio = params['precio'] || 0; // Aquí asegúrate de que `precio` es un número
     });
-  }
 
+  }
 }
